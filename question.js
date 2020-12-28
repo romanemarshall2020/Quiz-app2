908b817512e9b24c799d41256fe8167fa09395f9
romanemarshall <66698258+romanemarshall@users.noreply.github.com>
2020-12-28 10:15:51 -0500

    nextidx still not defined

--- question.js
+++ question.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function() {
 	// let randomNumber = Math.floor(Math.random() * 10);
 	let points = 0;
@@ -6,7 +5,6 @@ $(document).ready(function() {
 	const questions = [];
 	const correct = [];
 	const count = 0;
-	
 
 	let url = 'https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=boolean';
 	// let query = $('.start-btn').on('click', fetch(url));
@@ -19,11 +17,9 @@ $(document).ready(function() {
 			$('.render').show();
 		},
 
-		// complete: function () {
-
-		// },
+		
 		success: function(response) {
-			const length = questions.length
+			const length = questions.length;
 			// click event
 			$('.start-btn').on('click', function(e) {
 				// removes hide class from quiz box, revealing it on the screen
@@ -33,56 +29,73 @@ $(document).ready(function() {
 				e.preventDefault();
 				for (let result of response.results) {
 					// pushes questions into an array for later use
-                    questions.push(result.question);
-                    // pushes correct anwer to question into correct for later use
+					questions.push(result.question);
+					// pushes correct anwer to question into correct for later use
 					correct.push(result.correct_answer);
 				}
 				console.log(questions);
 				console.log(correct);
 				displayQuest();
- 
-
-				function nextidx(idx = 0, length, direction) {
-                    $('.btn').on('click', function(e) {
-                        e.preventDefault();
-                        for (let i = 0; i < questions.length; i++) {
-                            number = count + questions[i] ++;
-                            return number
-                        }
-                    })
 				
-				}
-				nextidx();
 			});
-			function displayQuest(number) {
-				for (let i = 0; i < questions.length; i++) {
-					let output = '';
-					output += `
-     <div class="quiz-box custom-box  ">
-     <div class="question-number">
-                    ${count} of ${questions.length}
-     </div>
-     <div class="question-text">
-     ${questions[i]}
-     </div>
-     <div class="option-container">
-         <div class="option" value="true">True</div>
-         <div class="option" value="false">False</div>
-     </div>
+			
 
-    <div class="next-question-btn">
-         <button onclick=nextQuestion type="button" class="btn">Next</button>
-    </div>
-                     `;
-                     
-                    //  console.log(totalquest)
-
-                    $('.render').html(output);
-                    
-                    
-				}
-			}
-			// displayQuest();
+			
 		}
 	});
+
+
+	function displayQuest(nextIdx) {
+
+		for (let i = 0; i < questions.length; i++) {
+			let output = '';
+			output += `
+<div class="quiz-box custom-box  ">
+<div class="question-number">
+			${count} of ${questions.length}
+</div>
+<div class="question-text">
+${questions[i]}
+</div>
+<div class="option-container">
+ <div class="option" value="true">True</div>
+ <div class="option" value="false">False</div>
+</div>
+
+<div class="next-question-btn">
+ <button onclick="nextIdx('.btn')" type="button" class="btn">Next</button>
+</div>
+			 `;
+
+			$('.render').html(output);
+		
+			
+		}
+		
+		
+	}
+
+
+	function nextIdx(idx = 0, length, direction) {
+		switch (direction) {
+			case 'Next': return (idx + 1) % length
+			default: return idx
+		}
+	}
+
+
+	let idx
+	function newIndexRender(direction) {
+		idx = nextIdx(idx, length, direction);
+		$('.question-text').html(questions[idx])
+	} 
+
+	newIndexRender()
+
+
+
+	
+	
+	
+	
 });
