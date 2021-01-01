f0324c210e506d8740d0e92dd3271b3ef03ac20b
romanemarshall <66698258+romanemarshall@users.noreply.github.com>
2021-01-01 08:31:34 -0500

    morning commit

--- question.js
+++ question.js
@@ -4,7 +4,9 @@ $(document).ready(function() {
 	// Array of Questions
 	const questions = [];
 	const correct = [];
+	const incorrect = []
 	let count = 0;
+	let answerCount = 0
 
 	let url = 'https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=boolean';
 	$.ajax({
@@ -12,9 +14,7 @@ $(document).ready(function() {
 		method: 'GET',
 		datatype: 'json',
 
-		
 		success: function(response) {
-			
 			// click event
 			$('.start-btn').on('click', function(e) {
 				// removes hide class from quiz box, revealing it on the screen
@@ -25,45 +25,88 @@ $(document).ready(function() {
 				for (let result of response.results) {
 					// pushes questions into an array for later use
 					questions.push(result.question);
-					// pushes correct anwer to question into correct for later use
+					// pushes correct answer to question into correct for later use
 					correct.push(result.correct_answer);
+					// pushes incorrect answers into incorrect for later use
+					incorrect.push(result.incorrect_answers)
 				}
 				console.log(questions);
 				console.log(correct);
+				console.log(incorrect)
 				displayQuest();
-				
+				answer()
+				check()
 			});
-			
-
-			
 		}
 	});
 
-
 	function displayQuest() {
-
 		for (let i = 0; i <= questions.length; i++) {
-			questions.forEach(element => {
-				$('.question-text').html(`${element}`)
-				$('.question-number').html(`${count} of ${questions.length}`)
+			questions.forEach((element) => {
+				$('.question-text').html(`${element}`);
+				$('.question-number').html(`${count} of ${questions.length}`);
 			});
-		
 		}
-		
+	
+	}
+	function answer() {
+	for(let i = 0; i <= correct.length; i++) {
+		correct.forEach((element) => {
+			trueAnswer = element[i]
+		//  if(element[i] = "true") {
+		// 	 $('#true').on('click', function() {
+		// 			// alert('you are correct')
+		// 			console.log('your correct')
+		// 	 }) 
+		//  } else {$('#fasle').on('click', function(){
+		// 	//  alert('you are incorrect')
+		// 	 console.log('you are incorrect')
+		//  })}
+		})
+	}
+}
+	//my next question function
+	$('.next-btn').on('click', function() {
+		for (let i = 0; i < questions.length - 1; i++) {
+			if (count < questions[i].length) {
+				count++;
+				console.log(questions[count]);
+
+				return $('.question-text').html(`${questions[count]}`);
+			}
+			else {
+				return alert('All Questions Completed')
+			} 
+		}
+	});
+
+	function check(trueAnswer) {
+	for(let i = 0; i <= correct.length - 1; i++) {
+			if (answerCount < correct[i].length){
+				answerCount++
+				console.log(correct[answerCount])
+				let playerChoice = $('#true').on('click') || $('#false').on('click')
+			if(playerChoice = trueAnswer) {
+				console.log('you chose correct')
+			} else if(playerChoice != trueAnswer) {
+				console.log('you are incorrect')
+			}
+			
+			// WrongAnser != trueAnswer
+			
+			}
+	}
+	// console.log(correctAnswer)
+
 	}
 	
-	$('.next-btn').on('click', function() {
-		for (let i = 0; i < questions.length -1; i++){
-		if (count < questions[i].length) {
-			count++
-			console.log(questions[count])
-			
-			return $('.question-text').html(`${questions[count]}`)
 
-		}
-		}
+// $('#true').on('click', function() {
+// 	$(".question-text").html("question is true")
+// })
 
-		
-		
-	})
+// $('#false').on('click', function() {
+// 	$(".question-text").html("question is false")
+// })
+
 });
